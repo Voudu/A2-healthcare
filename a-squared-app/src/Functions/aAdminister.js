ff0d02e2b9628daf98bc3174526b13a7c85c4f8b
Voudu <alexescatel9@gmail.com>
2023-01-16 14:04:30 -0600

    add source files

---
+++ a-squared-app/src/Functions/aAdminister.js
@@ -0,0 +1,26 @@
+/*
+
+
+constraints:
+    * will only consider used of the following medications:
+        * Intravaneous Agents:
+            - Propofol (common)
+                * allergy concerns: eggs, egg products, soybeans, soy products
+            - Ketamine
+                * allergy concerns: ketamine
+                * health concerns: untreated or uncontrolled hypertension
+            - Etomidate
+
+    * 2 user profiles 
+sources:
+    https://anesthesia.ucsf.edu/sites/anesthesia.ucsf.edu/files/wysiwyg/Anesthesia_Pocket_Guide_2020.pdf
+    https://www.aegisanesthesiapartners.com/common-medications-used-anesthesia/
+    https://www.drugs.com/dosage/propofol.html
+    https://www.drugs.com/mtm/ketamine.html
+    https://www.drugs.com/cdi/etomidate.html
+*/
+
+void startAnesthesiaDiagnostics(patient)
+{
+
+}
